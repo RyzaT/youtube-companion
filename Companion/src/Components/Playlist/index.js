c68e0fc48a34609f4aada099eb2666b3f23973f4
unknown <sobakoved@inbox.lv>
2023-03-14 22:27:32 +0000

    Adds npm pack for Youtube, adds basic functionality to get video ID

--- Companion/src/Components/Playlist/index.js
+++ Companion/src/Components/Playlist/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import YouTube from 'react-youtube';
+import Searchbar from "../Searchbar";
 import "./style.css";
+
+
+
 
 function Playlist() {
   return (
-    <div>Playlist section</div>
+    <div>
+      <p>Playlist </p>
+      
+      </div>
+    
   );
 }
 
