5f926b4b17aca2529b4445b968cdf4cb8942ab54
unknown <sobakoved@inbox.lv>
2023-03-16 17:27:18 +0000

    adds profile JSON, reads profile data

--- Companion/src/Components/Profile/index.js
+++ Companion/src/Components/Profile/index.js
@@ -1,9 +1,42 @@
 import React from "react";
+import profiles from '../../Profiles/Profile.json'
 import "./style.css";
+
+// generate buttons for each profile
+  const listItems = profiles.map((d) => <button onClick={() => showlists(d.title)} key={d.title}>{d.title}  </button>);
+    
+ 
+  function showlists(value){
+   // alert(value)
+   // const found = profiles.find(element => element === value);
+   // alert(found)
+    function isCherries(fruit) {
+      return fruit.title === value;
+ 
+ 
+  }
+
+
+  let enot = profiles.find(isCherries)
+  alert(value+enot.links.lists)
+  
+}
+
+
+
+//console.log((profiles.find(isCherries)));
+
+
+
 
 function Profile() {
   return (
-    <div>Profile section</div>
+    <div>
+      <p>Profiles from JSON (alternative to local storage)</p>
+    <ul className="list-group list-group-flush">
+          {listItems}
+        </ul>
+        </div>
   );
 }
 
