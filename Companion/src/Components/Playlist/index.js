5ba5812b9465d9d093432e61a817d75ec8576b91
RyzaT <ryantaylor50@hotmail.com>
2023-03-16 15:44:58 +0000

    Adds dynamic playlist container

--- Companion/src/Components/Playlist/index.js
+++ Companion/src/Components/Playlist/index.js
@@ -1,31 +1,18 @@
 import React from "react";
-import ListCard from '../Card_Playlist'
+import ListCard, {props} from '../Card_Playlist'
 import Searchbar from "../Searchbar";
+
 //import { playLists } from "../Searchbar";
 // this should probably return a list from local storage as it looks impossible ot update imported array fro Search bar. If anyone knows, contact me ))
 
 import "./style.css";
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-
-
-function Playlist() {
-
-
-  /* const listItems = playLists.map((number) =>
-    <ListCard listID={number}></ListCard> 
-  
-); */
-
-  return (
-    <div >
-{/* <p>{playLists.length}</p> */}
-<p>some text</p>
- 
-     </div>
-         
-       
-  );
+function Playlist(props) {
+  return <main className="container">{props.children}</main>;;
 }
+
 
 export default Playlist;
