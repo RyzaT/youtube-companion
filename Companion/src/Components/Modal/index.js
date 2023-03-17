7b38df547b18425d50a663e4002030d27b9e32e7
JayClay922 <jamesclayman922@live.com>
2023-03-17 13:26:21 +0000

    adding welcome modal

---
+++ Companion/src/Components/Modal/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+function WelcomeModal() {
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => setShow(false);
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Welcome to Your Youtube Companion</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>In order to create a playlist, please follow these steps:</p>
+        <ol>
+              <li>Create a playlist profile, eg. Gym, Coding, Meditation...</li>
+              <li>Type a keyword in the search bar, and click search eg. Rock, Rap, Classical...</li>
+              <li>When playlist is created, you can click the save button under a song to save for later use.</li>
+            </ol>
+      </Modal.Body>
+      <Modal.Footer style={{ textAlign: 'left' }}>
+        <p>Enjoy your playlist. Your Youtube, Your Way!</p>
+        <Button variant="primary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default WelcomeModal;
