9f0e9dee29ee6087cde58912a008f2160b09449e
JayClay922 <jamesclayman922@live.com>
2023-03-21 18:35:03 +0000

    updating width of logo and font weight of Application name

--- Companion/src/Components/Navbar/index.js
+++ Companion/src/Components/Navbar/index.js
@@ -8,8 +8,8 @@ function Navbar() {
   return (
     <nav class="navbar navbar-light bg-light w-100">
       <div class="container-fluid mx-auto my-auto">
-        <a class="navbar-brand mx-auto my-auto" href="#">
-          <img src={Logo} alt="" width="50" height="40" class="d-inline-block align-text-center"/>
+        <a class="navbar-brand mx-auto my-auto" href="#" style={{fontWeight: "bolder"}}>
+          <img src={Logo} alt="" width="35" height="45" class="d-inline-block align-text-center" className='navbar-text'/>
              Youtube Companion
         </a>
       </div>
