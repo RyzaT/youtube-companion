091742eafdb2e8cf5644932cef6a4061e6f6c991
JayClay922 <jamesclayman922@live.com>
2023-03-22 15:15:52 +0000

    updating navbar code

--- Companion/src/Components/Navbar/index.js
+++ Companion/src/Components/Navbar/index.js
@@ -6,10 +6,10 @@ import Logo from '../Navbar/Logo.png'
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-light bg-light w-100">
-      <div class="container-fluid mx-auto my-auto
-        <a class="navbar-brand mx-auto my-auto" href="#" style={{fontWeight: "bolder"}}>
-          <img src={Logo} alt="" width="35" height="45" class="d-inline-block align-text-center" className='navbar-text'/>
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid mx-auto my-auto">
+        <a class="navbar-brand mx-auto my-auto" href="/">
+          <img src={Logo} alt="" width="40" height="40" class="d-inline-block align-text-center"/>
              Youtube Companion
         </a>
       </div>
