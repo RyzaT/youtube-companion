9dc4eeefc6175670c93b7b5ac6326bcbda260962
RyzaT <ryantaylor50@hotmail.com>
2023-03-22 15:16:30 +0000

    Accidently  hit the keyboard!

--- Companion/src/Components/Navbar/index.js
+++ Companion/src/Components/Navbar/index.js
@@ -7,7 +7,7 @@ import Logo from '../Navbar/Logo.png'
 function Navbar() {
   return (
     <nav class="navbar navbar-light bg-light w-100">
-      <div class="container-fluid mx-auto my-auto
+      <div class="container-fluid mx-auto my-auto">
         <a class="navbar-brand mx-auto my-auto" href="#" style={{fontWeight: "bolder"}}>
           <img src={Logo} alt="" width="35" height="45" class="d-inline-block align-text-center" className='navbar-text'/>
              Youtube Companion
