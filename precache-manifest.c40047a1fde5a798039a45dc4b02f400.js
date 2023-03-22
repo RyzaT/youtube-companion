b0dd7447ecb7da0fc6d4f65a7df3a4ec54a75a7a
RyzaT <ryantaylor50@hotmail.com>
2023-03-22 13:38:35 +0000

    Updates

---
+++ precache-manifest.c40047a1fde5a798039a45dc4b02f400.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dbe359ec91a5322b45a",
+    "url": "/youtube-companion/static/css/main.b2913050.chunk.css"
+  },
+  {
+    "revision": "4dbe359ec91a5322b45a",
+    "url": "/youtube-companion/static/js/main.43c798cd.chunk.js"
+  },
+  {
+    "revision": "35d1c0b32cc6f33a12ea",
+    "url": "/youtube-companion/static/js/runtime~main.7c979c22.js"
+  },
+  {
+    "revision": "79504944a25909ebf904",
+    "url": "/youtube-companion/static/css/2.98f0d91b.chunk.css"
+  },
+  {
+    "revision": "79504944a25909ebf904",
+    "url": "/youtube-companion/static/js/2.61edafdc.chunk.js"
+  },
+  {
+    "revision": "3f0dc9c512cfed730230",
+    "url": "/youtube-companion/static/js/3.984a5bf2.chunk.js"
+  },
+  {
+    "revision": "63a3f51a4c641282e8211b12d703198b",
+    "url": "/youtube-companion/static/media/Logo.63a3f51a.png"
+  },
+  {
+    "revision": "e528ba623d5f9b09b3908e6e9370d0a3",
+    "url": "/youtube-companion/index.html"
+  }
+];
